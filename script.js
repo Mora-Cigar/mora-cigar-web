ca54bb6d54bde3858633bc3eee268deb336e860c
Mora-Cigar <moracigar@outlook.es>
2026-08-21 16:26:27 +0200

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-// Navegación suave y año automático.
-document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{const el=document.querySelector(a.getAttribute('href'));if(el){e.preventDefault();el.scrollIntoView({behavior:'smooth'});}}));
